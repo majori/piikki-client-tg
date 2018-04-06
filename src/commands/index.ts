dc80a36a5a7fc8e1330fbb00c20d44589b9be265
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-04-06 18:34:11 +0300

    Hotfix: add /start

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,6 +4,14 @@ import login from './login';
 import * as group from './group';
 
 export default (bot: any) => {
+  bot.command('/start', (ctx: any) => {
+    ctx.reply(
+      'Hello there! You can start by logging into your account with ' +
+      '`/login username password` command.',
+      { parse_mode: 'Markdown' },
+    );
+  });
+
   bot.command('/sub', transaction.subtract);
   bot.command('/add', transaction.add);
   bot.command('/nakki', transaction.effort);
