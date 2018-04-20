2a7a6b111d506e512da6bc25eb40f61090394547
Antti Sartjärvi <antti.sartjarvi@tietotemput.fi>
2018-04-20 21:39:59 +0300

    Create own files for help and start commands

---
+++ src/commands/help.ts
@@ -0,0 +1,11 @@
+export default async (ctx: any) => {
+  ctx.reply(
+    '/add `[amount]`\n - Adds saldo to your default group\n' +
+    '/sub `[amount]`\n - Subtracts saldo from your default group\n' +
+    '/nakki `[amount]`\n - Adds saldo to your default group as \'nakki\'\n' +
+    '/saldo\n - Lists your saldos in each group\n' +
+    '/login\n - Login to your Piikki account\n' +
+    '/setdefault\n - Sets your default group',
+    { parse_mode: 'Markdown' },
+  );
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,28 +1,13 @@
 import * as transaction from './transaction';
 import saldo from './saldo';
+import start from './start';
+import help from './help';
 import login from './login';
 import * as group from './group';
 
 export default (bot: any) => {
-  bot.command('/start', (ctx: any) => {
-    ctx.reply(
-      'Hello there! You can start by logging into your account with ' +
-      '`/login username password` command.',
-      { parse_mode: 'Markdown' },
-    );
-  });
-
-  bot.command('/help', (ctx: any) => {
-    ctx.reply(
-      '/add `[amount]`\n - Adds saldo to your default group\n' +
-      '/sub `[amount]`\n - Subtracts saldo from your default group\n' +
-      '/nakki `[amount]`\n - Adds saldo to your default group as \'nakki\'\n' +
-      '/saldo\n - Lists your saldos in each group\n' +
-      '/login\n - Login to your Piikki account\n' +
-      '/setdefault\n - Sets your default group',
-      { parse_mode: 'Markdown' },
-    );
-  });
+  bot.start(start);
+  bot.help(help);
 
   bot.command('/sub', transaction.subtract);
   bot.command('/add', transaction.add);
