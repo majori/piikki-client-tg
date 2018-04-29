0fea84ad42a4f2f22f9937eab89f47e7f864b5d0
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-04-29 14:37:10 +0300

    Add admin_user_id column

--- migrations/20180428182916_group_id_link.js
+++ migrations/20180428182916_group_id_link.js
@@ -1,8 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('groups', table => {
-    table.integer('tg_id')
+    table.integer('tg_group_id')
       .primary();
     table.integer('piikki_group_id')
+      .notNullable();
+    table.integer('admin_user_id')
       .notNullable();
   });
 };
