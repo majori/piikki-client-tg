030c6d9d0a6b0f78ef22fc98e0944ae29714368f
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-05-20 13:41:43 +0300

    Fix knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,31 +1,29 @@
-export default {
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-      host: 'localhost',
-      port: 5001,
-      database: 'postgres',
-      user: 'postgres',
-      password: 'password12!',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+export const development = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL || {
+    host: 'localhost',
+    port: 5001,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'password12!',
   },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+};
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+export const production = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
   },
 };
